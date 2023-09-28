fd948f46c0cf335364a83ab0eee54e4df340f9db
Yhucee14 <uchechukwu7814@gmail.com>
2023-09-28 13:04:39 -0700

    fixed extension display issue

---
+++ src/Popup.jsx
@@ -0,0 +1,9 @@
+
+
+const Popup = () => {
+  return (
+    <div>popup</div>
+  );
+}
+
+export default Popup;
---
+++ src/popmain.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom';
+import Popup from './Popup';
+
+const root = createRoot(document.getElementById("popup"));
+root.render(<Popup />);
